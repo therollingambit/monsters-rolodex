85d33e99363c51deec6add6c6ec1147fb833a859
Ahmad Enayathullah <ahmad.enayathullah@fdmgroup.com>
2021-04-13 17:40:38 +0800

    first commit

---
+++ src/components/card-list/CardList.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./card-list.styles.css";
+//Components
+import Card from '../card/Card';
+
+const CardList = (props) => {
+  return (
+    <div className="card-list">
+      {props.monsters.map((monster) => (
+        <Card key={monster.id} monster={monster} />
+      ))}
+    </div>
+  );
+};
+
+export default CardList;
